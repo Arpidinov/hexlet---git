8b15c34bcbe2dfea8ad9c3e4884b013f34bbfc2f
Arpidinov <93383219+Arpidinov@users.noreply.github.com>
2022-06-01 09:25:32 +0600

    Create bank.py

---
+++ bank.py
@@ -0,0 +1,27 @@
+
+class BankAccount:
+    # create the constuctor with parameters: accountNumber, name and balance 
+    def __init__(self,accountNumber, name, balance):
+        self.accountNumber = accountNumber
+        self.name = name
+        self.balance = balance
+        
+    # create Deposit() method
+    def Deposit(self , d ):
+        self.balance = self.balance + d
+    
+    # create Withdrawal method
+    def Withdrawal(self , w):
+        if(self.balance < w):
+            print("impossible operation! Insufficient balance !")
+        else:
+            self.balance = self.balance - w
+    # create bankFees() method
+    def bankFees(self):
+        self.balance = (95/100)*self.balance
+        
+    # create display() method
+    def display(self):
+        print("Account Number : " , self.accountNumber)
+        print("Account Name : " , self.name)
+        print("Account Balance : " , self.balance , " $")
